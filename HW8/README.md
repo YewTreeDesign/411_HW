1c9ee4cdc7f364484d18d186e09e08255cbbf9a4
Charlene Mundel <35701097+YewTreeDesign@users.noreply.github.com>
2019-03-15 10:00:15 -0600

    credit didn't get pushed

--- HW8/README.md
+++ HW8/README.md
@@ -1,2 +1,14 @@
 Live https://yewtreedesign.github.io/441_HW/HW8/index.html
 Repo https://github.com/YewTreeDesign/441_HW/tree/master/HW8
+
+Here's my resources for transitioning objects with in an array, they are pretty beneficial to look at when you are stuck. Because I had that problem.
+<br>
+<br>Resource Source Link For The Transitioning Words/Text:
+<br>Username: Paulpro
+<br>https://stackoverflow.com/questions/7425231/jquery-find-word-and-change-every-few-seconds/34640724
+<hr>
+Resource For Transitioning Fade Effect With Array of Images:<br>
+Username: James<br>
+https://stackoverflow.com/questions/2983957/animate-background-image-change-with-jquery
+<br>
+https://jsfiddle.net/srd76/36/<br>
--- HW8/script.js
+++ HW8/script.js
@@ -1,72 +1,118 @@
-//Preload images first
-$.fn.preload = function() {
-    this.each(function(){
+//Funny thing, this was sitting in my folder and it didn't get pushed. Thanks Github Desktop Hud.
+//Here's the Resource, this would benefit everyone if they are stuck.
+
+console.log("Here's my resources for transitioning objects with in an array, they are pretty beneficial to look at when you are stuck. Because I had that problem.");
+console.log("");
+console.log("Resource Source Link For The Transitioning Words/Text:");
+console.log("Username: Paulpro");
+console.log("https://stackoverflow.com/questions/7425231/jquery-find-word-and-change-every-few-seconds/34640724");
+console.log("   ");
+console.log("___________________________________________________________________________________________________");
+console.log("   ");
+console.log("Resource For Transitioning Fade Effect With Array of Images:");
+console.log("Username: James");
+console.log("https://stackoverflow.com/questions/2983957/animate-background-image-change-with-jquery");
+console.log("https://jsfiddle.net/srd76/36/");
+console.log("Check CSS to modify #background");
+console.log("");
+console.log("**************************************");
+console.log("");
+console.log("Have Fun!");
+
+
+$.fn.preload = function () {
+    this.each(function () {
         $('<img/>')[0].src = this;
     });
 }
-var images = Array("image/space-01.jpg",
-                   "image/space-02.jpg",
-                   "image/space-03.jpg",
-                   "image/space-04.jpg");
+var images = Array("image/space-01.jpg", "image/space-02.jpg", "image/space-03.jpg", "image/space-04.jpg");
 
-$([images[0],images[1],images[2],images[3]]).preload();
+$([images[0], images[1], images[2], images[3]])
+    .preload();
 
-// Usage:
 
+//Source For Transitoning images: https://stackoverflow.com/questions/2983957/animate-background-image-change-with-jquery + https://jsfiddle.net/srd76/36/
+//Username: James
 var currimg = 0;
 
-$(document).ready(function(){
+$(document)
+    .ready(function () {
 
-    function loadimg(){
+        function loadimg() {
 
-       $('#background').animate({ opacity: 1 }, 500,function(){
+            $('#background')
+                .animate({
+                    opacity: 1
+                }, 500, function () {
 
-            //finished animating, minifade out and fade new back in
-            $('#background').animate({ opacity: 0.7 }, 100,function(){
 
-                currimg++;
+                    $('#background')
+                        .animate({
+                            opacity: 0.7
+                        }, 100, function () {
 
-                if(currimg > images.length-1){
+                            currimg++;
 
-                    currimg=0;
+                            if (currimg > images.length - 1) {
 
-                }
+                                currimg = 0;
 
-                var newimage = images[currimg];
+                            }
 
-                //swap out bg src
-                $('#background').css("background-image", "url("+newimage+")");
+                            var newimage = images[currimg];
 
-                //animate fully back in
-                $('#background').animate({ opacity: 1 }, 400,function(){
+                            //swap out bg src
+                            $('#background')
+                                .css("background-image", "url(" + newimage + ")");
 
-                    //set timer for next
-                    setTimeout(loadimg,5000);
+                            //animate fully back in
+                            $('#background')
+                                .animate({
+                                    opacity: 1
+                                }, 400, function () {
+
+                                    //set timer for next
+                                    setTimeout(loadimg, 5000);
+
+                                });
+
+                        });
 
                 });
+                //Source For Transitoning images: https://stackoverflow.com/questions/2983957/animate-background-image-change-with-jquery + https://jsfiddle.net/srd76/36/
+                //Username: James
+        }
+        setTimeout(loadimg, 5000);
 
-            });
-
-        });
-
-     }
-     setTimeout(loadimg,5000);
-
-});
-
-(function(){
+    });
+// rosource Source Link:
+//https://stackoverflow.com/questions/7425231/jquery-find-word-and-change-every-few-seconds/34640724
+//Username: Paulpro
+(function () {
     var words = [
-        'Totality',
-        'Incredible',
-        'Lacuna',
-        'Array'
-        ], i = 0;
-    setInterval(function(){
-        $('#changerificwordspanid').fadeOut(function(){
-            $(this).html(words[i=(i+1)%words.length]).fadeIn();
-        });
-    }, 2000);
+        'Totality'
 
+
+            , 'Incredible'
+
+
+            , 'Lacuna'
+
+
+            , 'Array'
+        ],
+        i = 0;
+    setInterval(function () {
+        $('#changerificwordspanid')
+            .fadeOut(function () {
+                $(this)
+                    .html(words[i = (i + 1) % words.length])
+                    .fadeIn();
+            });
+    }, 2000);
+    // rosource Source Link:
+    //https://stackoverflow.com/questions/7425231/jquery-find-word-and-change-every-few-seconds/34640724
+    //Username: Paulpro
 })();
 
 var t = 0;
@@ -79,17 +125,25 @@ function moveit() {
     var ycenter = 385;
     var newLeft = Math.floor(xcenter + (r * Math.cos(t)));
     var newTop = Math.floor(ycenter + (r * Math.sin(t)));
-    $('#friends').animate({
-        top: newTop,
-        left: newLeft,
-    }, 1, function() {
-        moveit();
-    });
+    $('#friends')
+        .animate({
+            top: newTop,
+            left: newLeft,
+        }, 1, function () {
+            moveit();
+        });
 }
 
-$(document).ready(function() {
-    moveit();
-});
-$(document).mousemove(function(e){
-    $("#friend").stop().animate({left:e.pageX, top:e.pageY});
-});
+$(document)
+    .ready(function () {
+        moveit();
+    });
+$(document)
+    .mousemove(function (e) {
+        $("#friend")
+            .stop()
+            .animate({
+                left: e.pageX,
+                top: e.pageY
+            });
+    });
